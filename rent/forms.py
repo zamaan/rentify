06a76988435d266108e71c00470b2e237aff19b0
Zaman <Zaman@Zamans-MacBook-Air.local>
2016-03-07 18:06:34 +0530

    add image form

--- rent/forms.py
+++ rent/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from rent.models import Item
+from rent.models import Item,Upload
 
 class ItemForm(ModelForm):
     class Meta:
@@ -8,3 +8,7 @@ class ItemForm(ModelForm):
         fields = ('name', 'description','price','location')
 
 
+class UploadForm(ModelForm):
+	class Meta:
+		model=Upload
+		fields=('image',)
