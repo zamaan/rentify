1ef25f74a252bd5d10a358f6013fab15a68ab2fe
Zaman <Zaman@Zamans-MacBook-Air.local>
2016-03-07 18:06:54 +0530

    adding image

--- rent/models.py
+++ rent/models.py
@@ -8,3 +8,14 @@ class Item(models.Model):
 	price=models.IntegerField(null=True)
 	location=models.CharField(max_length=100,null=True)
 	slug=models.SlugField(unique=True,null=True)
+
+
+def __unicode__(self):
+    return self.name
+
+def get_image_path(instance,filename):
+	return '/'.join(['item_images', instance.item.slug,filename])
+
+class Upload(models.Model):
+	item=models.ForeignKey(Item,related_name="uploads")
+	image=models.ImageField(upload_to=get_image_path)
