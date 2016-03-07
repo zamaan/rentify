0df0c036e976a791db1963c873c325b019827345
Zaman <Zaman@Zamans-MacBook-Air.local>
2016-03-07 18:06:17 +0530

    to add image

--- rent/admin.py
+++ rent/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 #Register your models here.
-from rent.models import Item
+from rent.models import Item,Upload
 
 class ItemAdmin(admin.ModelAdmin):
 	model=Item
 	list_display=('name','description','price','location',)
 	prepopulated_fields={'slug':('name',)}
 
+class UploadAdmin(admin.ModelAdmin):
+	list_display=('item',)
+	list_display_links=( 'item',)
+
 admin.site.register(Item, ItemAdmin)
+admin.site.register(Upload, UploadAdmin)
