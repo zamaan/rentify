89c52dc4414da0828c37da72769949a5dc422c2a
Zaman <Zaman@Zamans-MacBook-Air.local>
2016-03-26 12:23:37 +0530

    adds user field on item model

--- rent/models.py
+++ rent/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-	name=models.CharField(max_length=255,null=True)
-	image=models.ImageField(null=True,blank=True)
-	description=models.TextField(null=True)
-	price=models.IntegerField(null=True)
-	location=models.CharField(max_length=100,null=True)
-	slug=models.SlugField(unique=True,null=True)
+    user = models.ForeignKey(User)
+    name=models.CharField(max_length=255,null=True)
+    image=models.ImageField(null=True,blank=True)
+    description=models.TextField(null=True)
+    price=models.IntegerField(null=True)
+    location=models.CharField(max_length=100,null=True)
+    slug=models.SlugField(unique=True,null=True)
 
-def __unicode__(self):
-    return self.name
+    def __unicode__(self):
+        return self.name
 
 def get_image_path(instance,filename):
 	return '/'.join(['item_images', instance.item.slug,filename])
