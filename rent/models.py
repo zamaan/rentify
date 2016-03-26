ddc4d0a17c325730e7f2c9d2acb1f2753edc9243
Zaman <Zaman@Zamans-MacBook-Air.local>
2016-03-26 12:26:26 +0530

    adds RequestItem model with rental_price method

--- rent/models.py
+++ rent/models.py
@@ -1,5 +1,13 @@
 #from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
+
+DURATION = (  
+    (1, '1 week'),
+    (2, '2 weeks'),
+    (3, '3 weeks'),
+    (4, '1 month'),
+)
 
 # Create your models here.
 class Item(models.Model):
@@ -14,9 +22,14 @@ class Item(models.Model):
     def __unicode__(self):
         return self.name
 
-def get_image_path(instance,filename):
-	return '/'.join(['item_images', instance.item.slug,filename])
+class RequestItem(models.Model):
+    item = models.ForeignKey(Item)
+    user = models.ForeignKey(User)
+    duration = models.IntegerField(max_length=50, choices=DURATION)
+    status = models.CharField(max_length=100)
 
-def get_absolute_url(self):
-    	return "/item/%s/" %self.slug
+    def __unicode__(self):
+        return "%s - %s"%(self.item.name,self.user.username)
 
+    def get_rental_price(self):
+        return self.item.price * self.duration
